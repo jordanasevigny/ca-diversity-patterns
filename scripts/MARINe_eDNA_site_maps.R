28154987464dcb4d9596584fb35f28ef06bc3921
jordanasevigny <jsevigny@ucsc.edu>
2025-01-14 09:24:54 -0800

    updated script with file placement changes

--- scripts/MARINe_eDNA_site_maps.R
+++ scripts/MARINe_eDNA_site_maps.R
@@ -16,13 +16,13 @@ class(world)
 # 2001 to 2003
 
 # load in data
-MARINe_df <- data.frame(read.csv("./data/filtered_MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
+MARINe_df <- data.frame(read.csv("./data/preliminary_processed_data/filtered_MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
   filter(year %in% c(2001, 2002, 2003)) %>%
   select(c("marine_site_name", "latitude", "longitude")) %>%
   distinct() %>%
   rename(Sites = marine_site_name, Latitude = latitude, Longitude = longitude) %>%
   mutate(Source = "MARINe")
-#write.csv(MARINe_df, 'filtered_MARINe_sites_2001-2003.csv')
+#write.csv(MARINe_df, './data/preliminary_processed_data/filtered_MARINe_sites_2001-2003.csv')
 
 eDNA_site_df <- data.frame(read.csv("./data/eDNA_sites.csv", header=TRUE)) %>%
   select(c("Sites", "Latitude", "Longitude")) %>%
@@ -38,18 +38,18 @@ ggplot(data = world) +
   ggtitle("eDNA Summer 2024 and MARINe 2001-2003 survey sites") +
   coord_sf(xlim = c(-110, -130), ylim = c(25, 45), expand = FALSE) +
   facet_wrap(~ Source) 
-ggsave("filtered_MARINe_2001-2003_eDNA_site_map.png")
+ggsave("./preliminary_figures/filtered_MARINe_2001-2003_eDNA_site_map.png")
 
 # 2001 only
 
 # load in data
-MARINe_df <- data.frame(read.csv("./data/MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
+MARINe_df <- data.frame(read.csv("./data/preliminary_processed_data/MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
   filter(year %in% 2001) %>%
   select(c("marine_site_name", "latitude", "longitude")) %>%
   distinct() %>%
   rename(Sites = marine_site_name, Latitude = latitude, Longitude = longitude) %>%
   mutate(Source = "MARINe")
-eDNA_site_df <- data.frame(read.csv("./data/eDNA_sites.csv", header=TRUE)) %>%
+eDNA_site_df <- data.frame(read.csv("./data/preliminary_processed_data/eDNA_sites.csv", header=TRUE)) %>%
   select(c("Sites", "Latitude", "Longitude")) %>%
   mutate(Source = "eDNA")
 
@@ -63,12 +63,12 @@ ggplot(data = world) +
   ggtitle("MARINe 2001 and eDNA survey sites") +
   coord_sf(xlim = c(-110, -130), ylim = c(25, 45), expand = FALSE) +
   facet_wrap(~ Source) 
-ggsave("MARINe_2001_eDNA_site_map.png")
+ggsave("preliminary_figures/MARINe_2001_eDNA_site_map.png")
 
 # 2002 only
 
 # load in data
-MARINe_df <- data.frame(read.csv("./data/MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
+MARINe_df <- data.frame(read.csv("./data/preliminary_processed_data/MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
   filter(year %in% 2002) %>%
   select(c("marine_site_name", "latitude", "longitude")) %>%
   distinct() %>%
@@ -88,18 +88,18 @@ ggplot(data = world) +
   ggtitle("MARINe 2002 and eDNA survey sites") +
   coord_sf(xlim = c(-110, -130), ylim = c(25, 45), expand = FALSE) +
   facet_wrap(~ Source) 
-ggsave("MARINe_2002_eDNA_site_map.png")
+ggsave("preliminary_figures/MARINe_2002_eDNA_site_map.png")
 
 # 2003 only
 
 # load in data
-MARINe_df <- data.frame(read.csv("./data/MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
+MARINe_df <- data.frame(read.csv("./data/preliminary_processed_data/MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
   filter(year %in% 2003) %>%
   select(c("marine_site_name", "latitude", "longitude")) %>%
   distinct() %>%
   rename(Sites = marine_site_name, Latitude = latitude, Longitude = longitude) %>%
   mutate(Source = "MARINe")
-eDNA_site_df <- data.frame(read.csv("./data/eDNA_sites.csv", header=TRUE)) %>%
+eDNA_site_df <- data.frame(read.csv("./data/preliminary_processed_data/eDNA_sites.csv", header=TRUE)) %>%
   select(c("Sites", "Latitude", "Longitude")) %>%
   mutate(Source = "eDNA")
 
@@ -113,11 +113,11 @@ ggplot(data = world) +
   ggtitle("MARINe 2003 and eDNA survey sites") +
   coord_sf(xlim = c(-110, -130), ylim = c(25, 45), expand = FALSE) +
   facet_wrap(~ Source) 
-ggsave("MARINe_2003_eDNA_site_map.png")
+ggsave("preliminary_figures/MARINe_2003_eDNA_site_map.png")
 
 
 # are any of the 2001 to 2003 sites duplicates?
-MARINe_df <- data.frame(read.csv("./data/MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
+MARINe_df <- data.frame(read.csv("./data/preliminary_processed_data/MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
   filter(year %in% c(2001, 2002, 2003)) %>%
   select(c("marine_site_name", "latitude", "longitude", "year", "sample_date")) %>%
   distinct()
@@ -134,13 +134,13 @@ ggplot(data = world) +
   xlab("Longitude") + ylab("Latitude") +
   ggtitle("MARINe 2001-2003 survey sites colored by survey #") +
   coord_sf(xlim = c(-110, -130), ylim = c(25, 45), expand = FALSE)
-ggsave("MARINe_count_2001-2003_eDNA_site_map.png")
+ggsave("preliminary_figures/MARINe_count_2001-2003_eDNA_site_map.png")
 
 
 # marine map of each year's surveys
 
 # load in data
-MARINe_df <- data.frame(read.csv("./data/MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
+MARINe_df <- data.frame(read.csv("./data/preliminary_processed_data/MARINe_survey_taxa_dmy.csv", header=TRUE)) %>%
   select(c("marine_site_name", "latitude", "longitude", "year")) %>%
   distinct() %>%
   arrange(year) %>%
@@ -157,13 +157,13 @@ for(yr in survey_years) {
     coord_sf(xlim = c(-110, -130), ylim = c(25, 45), expand = FALSE)
   
   print(p)
-  ggsave(filename = paste0("MARINe_Sites_map_", yr, ".png"), 
+  ggsave(filename = paste0("preliminary_figures/MARINe_Sites_map_", yr, ".png"), 
          plot = p)
 }
 
 # edna only map
 
-eDNA_site_df <- data.frame(read.csv("./data/eDNA_sites.csv", header=TRUE)) %>%
+eDNA_site_df <- data.frame(read.csv("./data/preliminary_processed_data/eDNA_sites.csv", header=TRUE)) %>%
   select(c("Sites", "Latitude", "Longitude")) %>%
   mutate(Source = "eDNA")
 
@@ -173,4 +173,4 @@ ggplot(data = world) +
   xlab("Longitude") + ylab("Latitude") +
   ggtitle("eDNA survey sites") +
   coord_sf(xlim = c(-110, -130), ylim = c(25, 45), expand = FALSE)
-ggsave("eDNA_site_map.png")
+ggsave("preliminary_figures/eDNA_site_map.png")
