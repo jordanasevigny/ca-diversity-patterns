7dc7613b0c2ccbf199e0628a2b4a1be15d70432b
jordanasevigny <jsevigny@ucsc.edu>
2025-05-13 15:32:54 -0700

    made maps specific to chapters

--- scripts/figures/map_marine_PCE_PECO_polys.py
+++ scripts/figures/map_marine_PCE_PECO_polys.py
@@ -30,7 +30,7 @@ lat_rho = grid_ds.variables['lat_rho'][:]
 lon_rho = grid_ds.variables['lon_rho'][:]
 
 # BL ORIGIN POLYGONS
-points_ex_data = pd.read_csv('/home/jsevigny/git/CA_diversity_patterns/data/coast_polygons/wc15n_300km2_settling_polygons.txt', delimiter=',')
+points_ex_data = pd.read_csv('/home/jsevigny/git/ca-diversity-patterns/data/coast_polygons/wc15n_300km2_settling_polygons.txt', delimiter=',')
 # Make the vertices into polygons
 # Function that groups the lat and lon of the verteces then zips them into polygons
 def create_polygon(group):
@@ -71,7 +71,7 @@ plt.show()
 # MARINe Sites
 
 # Load the CSV file into a DataFrame
-marine = pd.read_csv('/home/jsevigny/git/CA_diversity_patterns/data/processed_data/biodiversity/marine_species_20241025_dates_20241028_merged.csv')
+marine = pd.read_csv('/home/jsevigny/git/ca-diversity-patterns/data/processed_data/biodiversity/marine_species_20241025_dates_20241028_merged.csv')
 
 # Remove duplicates based on latitude and longitude
 unique_coords_marine = marine[['latitude', 'longitude']].drop_duplicates()
@@ -92,7 +92,7 @@ plt.show()
 # MARINE Sites Extended
 # Specify the ZIP file and the CSV file inside it
 
-zf = zipfile.ZipFile('/home/jsevigny/git/CA_diversity_patterns/data/marine_data/gbif_marine_biodiversity_20250409.zip') 
+zf = zipfile.ZipFile('/home/jsevigny/git/ca-diversity-patterns/data/marine_data/gbif_marine_biodiversity_20250409.zip') 
 marine_ex = pd.read_csv(zf.open('0014067-250402121839773.csv'), sep='\t')
 
 # Remove duplicates based on latitude and longitude
@@ -114,7 +114,7 @@ plt.show()
 # PCE (PC-eDNA) Sites
 
 # Load the CSV file into a DataFrame
-PCE = pd.read_csv('/home/jsevigny/git/CA_diversity_patterns/data/edna_jv_data/summer_2024/Summer_Exped_And_Central_Coast_All_Metadata_2024.csv')
+PCE = pd.read_csv('/home/jsevigny/git/ca-diversity-patterns/data/edna_jv_data/summer_2024/Summer_Exped_And_Central_Coast_All_Metadata_2024.csv')
 
 # Drop odd sample sites (inland)
 PCE = PCE.dropna(subset=['WP'])
@@ -138,7 +138,7 @@ plt.show()
 # PECO Sites
 
 # Load the CSV file into a DataFrame
-peco = pd.read_csv('/home/jsevigny/git/CA_diversity_patterns/data/peco_data/peco_sites_manual.csv')
+peco = pd.read_csv('/home/jsevigny/git/ca-diversity-patterns/data/peco_data/peco_sites_manual.csv')
 
 # Remove duplicates based on latitude and longitude
 unique_coords_peco = peco[['Latitude', 'Longitude']].drop_duplicates()
@@ -194,7 +194,7 @@ legend_elements = [
     Line2D([0], [0], marker='*', color='w', label='MARINe Sites', markerfacecolor='#00BFC4', markersize=10),
     Line2D([0], [0], marker='*', color='w', label='PCE Sites', markerfacecolor='#F8766D', markersize=10),
     Line2D([0], [0], marker='*', color='w', label='PECO Sites', markerfacecolor='#C77CFF', markersize=10),
-    Line2D([0], [0], color='black', lw=1, label='Settlement Cells')
+    Line2D([0], [0], color='black', lw=1, label='Release/Settlement Cells')
 ]
 
 # Add the legend to the plot
@@ -213,8 +213,137 @@ ax.set_yticklabels([f"{i}°" for i in range(25, 60, 3)])  # Latitude labels
 
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
-plt.title("Settlement Cells & Sample Sites")
+plt.title("Release/Settlement Cells & Sample Sites")
 
-plt.savefig("/home/jsevigny/git/CA_diversity_patterns/figures/biodiversity/map_marine_PCE_PECO_polys.pdf", bbox_inches='tight')
+plt.savefig("/home/jsevigny/git/ca-diversity-patterns/figures/biodiversity/map_marine_PCE_PECO_polys.pdf", bbox_inches='tight')
 plt.show()
 
+#%%
+# Map for Chapter 2
+
+fig, ax = plt.subplots(figsize=(12, 8), subplot_kw={'projection': ccrs.PlateCarree()})
+
+# Add coastlines to the map
+#ax.coastlines(resolution='110m', color='black', linewidth=0.2)
+# Optional: Add additional geographical features (e.g., land, rivers)
+ax.add_feature(cfeature.LAND, edgecolor='black', linewidth=0.2)
+ax.add_feature(cfeature.BORDERS, linestyle=':', linewidth=0.1, edgecolor='gray')
+ax.add_feature(cfeature.STATES, linestyle='-', linewidth=0.1, edgecolor='gray')
+
+# Add sample sites
+# Filter the MARINe to only rows where latitude < 42 (~PCE bounds)
+filtered = unique_coords_marine_ex[unique_coords_marine_ex['decimalLatitude'] < 38]
+ax.scatter(filtered['decimalLongitude'], filtered['decimalLatitude'], c='#00BFC4', marker='*')
+#ax.scatter(unique_coords_peco['Longitude'], unique_coords_peco['Latitude'], c='#C77CFF', marker='*')
+ax.scatter(unique_coords_PCE['Longitude'], unique_coords_PCE['Latitude'], c='#F8766D', marker='*')
+
+# Add settlement polygons
+# Loop through all rows in the polygon DataFrame
+for idx, row in polygon_df.iterrows():
+    polygon = row['polygon']
+    
+    # Limit settlement polygons to max lat of PCE
+    if polygon.bounds[3] < 38.0:  # bounds = (minx, miny, maxx, maxy)
+        x, y = polygon.exterior.xy
+        ax.plot(x, y, alpha=0.9, color='black', linewidth=0.5)
+    
+    # x, y = polygon.exterior.xy
+    # ax.plot(x, y, alpha=0.9, color='black', linewidth=0.5)
+
+# Adjust plot limits to fit all polygons
+ax.autoscale()  # Automatically scale the axes to fit the polygons
+ax.set_aspect('equal', adjustable='box')  # Keep aspect ratio equal for accurate representation
+
+# Custom legend handles
+legend_elements = [
+    Line2D([0], [0], marker='*', color='w', label='MARINe-CA Sites', markerfacecolor='#00BFC4', markersize=10),
+    Line2D([0], [0], marker='*', color='w', label='PCE Sites', markerfacecolor='#F8766D', markersize=10),
+  #  Line2D([0], [0], marker='*', color='w', label='PECO Sites', markerfacecolor='#C77CFF', markersize=10),
+    Line2D([0], [0], color='black', lw=1, label='Release/Settlement Cells')
+]
+
+# Add the legend to the plot
+ax.legend(handles=legend_elements, loc='lower left')  # or another location like 'upper right'
+
+# Add gridlines for latitude and longitude
+ax.grid(True, which='both', linestyle=':', color='gray', linewidth=0.5)
+
+# Customize latitude and longitude ticks and labels
+ax.set_xticks(range(-138, -110, 3))  # Longitude range (modify as needed)
+ax.set_yticks(range(25, 60, 3))  # Latitude range (modify as needed)
+
+# Optional: Customize tick labels
+ax.set_xticklabels([f"{i}°" for i in range(-138, -110, 3)])  # Longitude labels
+ax.set_yticklabels([f"{i}°" for i in range(25, 60, 3)])  # Latitude labels
+
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.title("Release/Settlement Cells & MARINe-CA and PCE Sample Sites")
+ax.set_extent([-126, -111, 25, 40], crs=ccrs.PlateCarree())
+
+#plt.savefig("/home/jsevigny/git/ca-diversity-patterns/figures/biodiversity/map_marine_PCE_polys.pdf", bbox_inches='tight')
+plt.show()
+
+#%%
+# Map for Chapter 3
+
+fig, ax = plt.subplots(figsize=(12, 8), subplot_kw={'projection': ccrs.PlateCarree()})
+
+# Add coastlines to the map
+#ax.coastlines(resolution='110m', color='black', linewidth=0.2)
+# Optional: Add additional geographical features (e.g., land, rivers)
+ax.add_feature(cfeature.LAND, edgecolor='black', linewidth=0.2)
+ax.add_feature(cfeature.BORDERS, linestyle=':', linewidth=0.1, edgecolor='gray')
+ax.add_feature(cfeature.STATES, linestyle='-', linewidth=0.1, edgecolor='gray')
+
+# Add sample sites
+ax.scatter(unique_coords_marine_ex['decimalLongitude'], unique_coords_marine_ex['decimalLatitude'], c='#00BFC4', marker='*')
+ax.scatter(unique_coords_peco['Longitude'], unique_coords_peco['Latitude'], c='#C77CFF', marker='*')
+#ax.scatter(unique_coords_PCE['Longitude'], unique_coords_PCE['Latitude'], c='#F8766D', marker='*')
+
+# Add settlement polygons
+# Loop through all rows in the polygon DataFrame
+# for idx, row in polygon_df.iterrows():
+#     polygon = row['polygon']
+    
+#     # Limit settlement polygons to max lat of PCE
+#     if polygon.bounds[3] < 38.0:  # bounds = (minx, miny, maxx, maxy)
+#         x, y = polygon.exterior.xy
+#         ax.plot(x, y, alpha=0.9, color='black', linewidth=0.5)
+    
+#     # x, y = polygon.exterior.xy
+#     # ax.plot(x, y, alpha=0.9, color='black', linewidth=0.5)
+
+# Adjust plot limits to fit all polygons
+ax.autoscale()  # Automatically scale the axes to fit the polygons
+ax.set_aspect('equal', adjustable='box')  # Keep aspect ratio equal for accurate representation
+
+# Custom legend handles
+legend_elements = [
+    Line2D([0], [0], marker='*', color='w', label='MARINe/PISCO Sites', markerfacecolor='#00BFC4', markersize=10),
+  #  Line2D([0], [0], marker='*', color='w', label='PCE Sites', markerfacecolor='#F8766D', markersize=10),
+    Line2D([0], [0], marker='*', color='w', label='PECO Sites', markerfacecolor='#C77CFF', markersize=10),
+  #  Line2D([0], [0], color='black', lw=1, label='Release/Settlement Cells')
+]
+
+# Add the legend to the plot
+ax.legend(handles=legend_elements, loc='lower left')  # or another location like 'upper right'
+
+# Add gridlines for latitude and longitude
+ax.grid(True, which='both', linestyle=':', color='gray', linewidth=0.5)
+
+# Customize latitude and longitude ticks and labels
+ax.set_xticks(range(-138, -110, 3))  # Longitude range (modify as needed)
+ax.set_yticks(range(25, 60, 3))  # Latitude range (modify as needed)
+
+# Optional: Customize tick labels
+ax.set_xticklabels([f"{i}°" for i in range(-138, -110, 3)])  # Longitude labels
+ax.set_yticklabels([f"{i}°" for i in range(25, 60, 3)])  # Latitude labels
+
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.title("Release/Settlement Cells & MARINe/PISCO and PECO Sample Sites")
+#ax.set_extent([-126, -111, 25, 40], crs=ccrs.PlateCarree())
+
+plt.savefig("/home/jsevigny/git/ca-diversity-patterns/figures/biodiversity/map_marine_peco.pdf", bbox_inches='tight')
+plt.show()
